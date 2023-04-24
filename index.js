541b2049ef79b9ff7ad6542e57fa5eed5e9642b0
JavatoX <cdpm98@hotmail.com>
2023-04-24 10:22:16 -0600

    archivo de vercel agregado

--- index.js
+++ index.js
@@ -15,9 +15,6 @@ const rutaUsuarios = require("./routes/usuarios.routes");
 
 const app = express(); //* Crear servidor.
 
-//* Habilitar carpetas estaticas.
-app.use(express.static(path.join("./public/uploads")));
-
 //* Configuraciones.
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -25,11 +22,16 @@ app.use(bodyParser.json());
 //* Habilitar CORS.
 app.use(cors());
 
+//* Habilitar carpetas estaticas.
+app.use(express.static(path.join("./public/uploads")));
+
 //* Habilitar rutas.
 app.use("/api/peliculas", rutaPeliculas);
 app.use("/api/usuarios", rutaUsuarios);
 
+const PORT = process.env.PORT || process.env.BACKEND_PORT;
+
 //* Iniciar Servidor.
-app.listen(process.env.BACKEND_PORT, () => {
-  console.log(`Servidor conectado en el puerto: ${process.env.BACKEND_PORT}`);
+app.listen(PORT, () => {
+  console.log(`Servidor conectado en el puerto: ${PORT}`);
 });
